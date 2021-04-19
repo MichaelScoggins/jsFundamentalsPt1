b27aa8716d9783be0619e2d7760b7387a0a38409
michaelscoggins <rhythmghosts@gmail.com>
2021-04-19 03:21:29 -0500

    first commit

---
+++ random_hex_color_code.js
@@ -0,0 +1,8 @@
+// Generate a random hexadecimal color code
+//#Source https://bit.ly/2neWfJ2
+const random_hex_color_code = () => {
+  let n = (Math.random() * 0xfffff * 1000000).toString(16);
+  return "#" + n.slice(0, 6);
+};
+
+console.log(random_hex_color_code()); // #2375d4
